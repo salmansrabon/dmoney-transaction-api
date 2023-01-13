6623fb6be5b8df14adc489ebdaed876b0c71d693
salmansrabon <salmansrabon@gmail.com>
2023-01-14 02:12:22 +0600

    added .env

--- api/sequelizeModel/db.js
+++ api/sequelizeModel/db.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config()
 
-const sequelize = new Sequelize('emoneydb', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
+const db_name = process.env.DB_NAME;
+const db_user = process.env.DB_USER;
+const host = process.env.DB_HOST;
+
+const sequelize = new Sequelize(db_name, db_user, '', {
+    host: host,
+    dialect: 'mysql',
+    logging: false
 });
 
 module.exports = { sequelize }
