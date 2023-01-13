d3dc38d9dbf3086bcddf3253d4e0bb5ec39f2276
salmansrabon <salmansrabon@gmail.com>
2023-01-14 02:54:11 +0600

    added custom env

--- api/sequelizeModel/db.js
+++ api/sequelizeModel/db.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config()
+require('custom-env').env()
+//require('custom-env').env('dev')
 
 const db_name = process.env.DB_NAME;
 const db_user = process.env.DB_USER;
 const host = process.env.DB_HOST;
+const password = process.env.DB_PASSWORD;
 
-const sequelize = new Sequelize(db_name, db_user, '', {
+const sequelize = new Sequelize(db_name, db_user, password, {
     host: host,
     dialect: 'mysql',
     logging: false
