5297691adaa337739ff08d7ad975a2ca27b2052d
Salman Srabon <48891202+salmansrabon@users.noreply.github.com>
2024-04-09 22:44:52 +0600

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -2,5 +2,5 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
-
+//test deployment 1
 module.exports = { swaggerDocument };
