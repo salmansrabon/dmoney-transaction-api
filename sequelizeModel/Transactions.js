cfd5cf0ebc120d3f89678af8c89f50dfdb2ef809
salmansrabon <salmansrabon@gmail.com>
2024-09-01 02:11:17 +0600

    added seed data when API restarted

--- sequelizeModel/Transactions.js
+++ sequelizeModel/Transactions.js
@@ -1,10 +1,8 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes, Op } = require('sequelize');
 const { sequelize } = require('./db');
 
-sequelize.sync();
-
-exports.Transactions = sequelize.define('Transactions', {
-
+// Define the Transactions model
+const Transactions = sequelize.define('Transactions', {
     account: {
         type: DataTypes.STRING,
         allowNull: false
@@ -23,7 +21,8 @@ exports.Transactions = sequelize.define('Transactions', {
     },
     trnxId: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: true
     },
     debit: {
         type: DataTypes.INTEGER,
@@ -33,5 +32,35 @@ exports.Transactions = sequelize.define('Transactions', {
         type: DataTypes.DOUBLE,
         allowNull: false
     }
-
 });
+
+// Seed data to be inserted if not found
+const seedTransaction = {
+    account: "SYSTEM",
+    from_account: "SUPER_USER",
+    to_account: "SYSTEM",
+    description: "SYSTEM DEPOSIT",
+    trnxId: "TRNX1001",
+    debit: 0,
+    credit: 10000000
+};
+
+// Sync database and conditionally insert seed data
+sequelize.sync().then(async () => {
+    // Check if the transaction with the specific trnxId exists
+    const existingTransaction = await Transactions.findOne({
+        where: {
+            trnxId: seedTransaction.trnxId
+        }
+    });
+
+    // If not found, insert the seed transaction
+    if (!existingTransaction) {
+        await Transactions.create(seedTransaction);
+        console.log('Seed SYSTEM account has been inserted.');
+    } else {
+        console.log('Seed SYSTEM account already exists and was not inserted.');
+    }
+});
+
+module.exports = { Transactions };
