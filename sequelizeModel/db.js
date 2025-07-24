cac4117321063ffd2ddff65c12fff5c4f5be5060
SalmanRahman <salman.rahman@sharebox.no>
2025-07-24 08:53:58 +0600

    feat: add support for custom database port in Sequelize configuration

--- sequelizeModel/db.js
+++ sequelizeModel/db.js
@@ -5,10 +5,12 @@ require('custom-env').env('dev')
 const db_name = process.env.DB_NAME;
 const db_user = process.env.DB_USER;
 const host = process.env.DB_HOST;
+const port=process.env.DB_PORT || 3306;
 const password = process.env.DB_PASSWORD;
 
 const sequelize = new Sequelize(db_name, db_user, password, {
     host: host,
+    port: port,
     dialect: 'mysql',
     logging: true
 });
